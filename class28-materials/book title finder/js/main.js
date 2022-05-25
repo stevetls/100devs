3eccce546fa886136bf2b6bf3d99be03d63f3f2d
stevetls <stevetsoi@outlook.com>
2022-05-25 17:33:36 +0200

    class 29 material

--- class28-materials/book title finder/js/main.js
+++ class28-materials/book title finder/js/main.js
@@ -5,14 +5,15 @@ document.querySelector('h3').innerText = localStorage.getItem('books')
 function getFetch(){
   const isbn = document.querySelector('input').value
   console.log(isbn)
-  const url = `https://openlibrary.org/isbn/${isbn}.json`
+  const url = `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=details&format=json`
+  //`https://openlibrary.org/isbn/${isbn}.json`
 
         
   fetch(url)
       .then(res => res.json()) // parse response as JSON
       .then(data => {
-        console.log(data.title)
-        localStorage.setItem('books', data.title)
+        console.log(data[ISBN:${isbn}].details)
+        localStorage.setItem('books', data[ISBN:${isbn}].details.title)
         document.querySelector('h3').innerText = localStorage.getItem('books')
 
       })
