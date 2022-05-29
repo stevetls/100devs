d92b7a51406b5e6b1886ea21bafd849a408b3087
stevetls <stevetsoi@outlook.com>
2022-05-29 22:41:42 +0200

    class 30 material

---
+++ class30-materials/objects-espresso-machine/js/main.js
@@ -0,0 +1,21 @@
+//Create an espresso machine class that makes machines with 4 properties and 3 methods
+
+class EspressoMachine{
+    constructor(brand, model, color, waterTankSize){
+        this.brand = brand
+        this.model = model
+        this.color = color
+        this.waterTankSize = waterTankSize
+    }
+        brew(){
+            console.log("brewing")
+        }
+        cleanme(){
+            console.log("it's time to clean me")
+        }
+        emptyTray(){
+            console.log("Please empty tray")
+        }
+    }
+
+let gaggia = new EspressoMachine('red', 'Gaggia', 'Classic Pro', 400)
